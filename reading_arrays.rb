fb518583ffb6db99dd4cc607e8e76293627a4293
zreown <zoe.reown@gmail.com>
2018-07-11 15:36:09 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -11,31 +11,31 @@ def first_student_by_index
   return = STUDENT_NAMES[0]
   
 end
-first_student_by_index
+# first_student_by_index
 
 def fourth_student_by_index
   return = STUDENTS_NAMES[3]
   
 end
-first_student_by_index
+# first_student_by_index
 
 def last_student_by_index
   return = STUDENTS_NAMES[5]
   
 end
-first_student_by_index
+# first_student_by_index
 
 def first_student_by_method
   return = STUDENTS_NAMES.first
 end
-first_student_by_index
+# first_student_by_index
 
 def last_student_by_method
   return = STUDENTS_NAMES.last
 end
-first_student_by_index
+# first_student_by_index
 
 def first_second_and_third_students
   return = STUDENTS_NAMES[0..2]
 end
-first_student_by_index
+# first_student_by_index
